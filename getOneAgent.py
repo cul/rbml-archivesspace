b0ce7d3b6beeb8aa5ef9b8b00bc874e6846f1d51
MyName <me@me.com>
2018-06-06 21:06:50 -0400

    grab one person agent json object

---
+++ getOneAgent.py
@@ -0,0 +1,27 @@
+import json
+import requests
+import secrets
+import time
+
+startTime = time.time()
+
+#call secrets for authentication
+baseURL = secrets.baseURL
+user = secrets.user
+password = secrets.password
+
+#authenticate session
+auth = requests.post(baseURL + '/users/'+user+'/login?password='+password).json()
+session = auth["session"]
+headers = {'X-ArchivesSpace-Session':session, 'Content_Type':'application/json'}
+print 'authenticated'
+
+id = input('Enter person agent id number, e.g. 2435: ')
+endpoint = '//agents/people/'+str(id)
+output = requests.get(baseURL + endpoint, headers=headers).json()
+print output
+
+elapsedTime = time.time() - startTime
+m, s = divmod(elapsedTime, 60)
+h, m = divmod(m, 60)
+print 'Total script run time: ', '%d:%02d:%02d' % (h, m, s)
