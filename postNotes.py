0585860efed774c1b5ea83c6f37adddad11a5e25
Kevin Schlottmann <kws2126@columbia.edu>
2018-07-31 14:40:45 -0400

    working script to add access note for unprocessed collections

--- postNotes.py
+++ postNotes.py
@@ -18,16 +18,37 @@ print "Is set to dev if URL contains dev:", baseURL
 print "This script will read list of AS IDs from input_AS_ids.csv and add the specified note. Press any key to continue, Ctrl-C to abort."
 raw_input("Press Enter to continue...")
 
+#create a dict with all the notes contents
+#to do : abstract and document this to make entering notes easier
+noteContents = [{'content': 'This is my new note. DEREK JETER.', 'publish': True, 'jsonmodel_type': 'note_text'}]
+localStatus = {'local_access_restriction_type': ['Available']}
+wholeNote = {'rights_restriction': localStatus, 'subnotes': noteContents, 'jsonmodel_type': 'note_multipart', 'publish': True, 'label': ' Restrictions on Access', 'type': 'accessrestrict'}
+
 with open('input_AS_ids.csv', 'rb') as csvfile:
-	reader = csv.reader(csvfile, delimiter=',', quotechar='"')
-	for row in reader:
-		ASID = row[0]
-    # Submit a get request for the archival object and store the JSON
-		archival_object_json = requests.get(baseURL+ASID,headers=headers).json()
-    # Add the new repository_processing_note to the existing archival object record. NB this will overwrite the existing note.
-#		archival_object_json['repository_processing_note'] = "This is the text of the newly added note. Paul O'Neill."
-#		archival_object_data = json.dumps(archival_object_json)
-		#print archival_object_data
-    # Repost the archival object containing the new note
-		#archival_object_update = requests.post(baseURL+ASID,headers=headers,data=archival_object_data).json()
-    		#print archival_object_update
+    reader = csv.reader(csvfile, delimiter=',', quotechar='"')
+    for row in reader:
+        ASID = row[0]
+        # Submit a get request for the archival object and store the JSON
+        archival_object_json = requests.get(baseURL+ASID,headers=headers).json()
+
+        # use this function to search inside notes list and return the index of first access subnote, so below we can insert the new one before it
+        def getIndex():
+            i = 0
+            for i in range(len(archival_object_json['notes'])):
+                if 'accessrestrict' in archival_object_json['notes'][i].values():
+                    return i
+                    break
+
+        #test for null index return and arbitrarily put it third
+        noteIndex = getIndex()
+        if noteIndex is None:
+            noteIndex = 3
+
+        #insert new note above into the right place in the notes list, before the existing access notes
+        archival_object_json['notes'].insert(noteIndex,wholeNote)
+
+          #prepare for repost
+        archival_object_data = json.dumps(archival_object_json)
+          # Repost the archival object containing the new note
+        archival_object_update = requests.post(baseURL+ASID,headers=headers,data=archival_object_data).json()
+        print archival_object_update
