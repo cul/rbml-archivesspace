d4525e146abba4f9f3f8531a30a82204f8209964
Kevin Schlottmann <kws2126@columbia.edu>
2018-08-06 14:39:22 -0400

    script to grab all user info

--- getUsers.py
+++ getUsers.py
@@ -16,22 +16,20 @@ session = auth["session"]
 headers = {'X-ArchivesSpace-Session':session, 'Content_Type':'application/json'}
 print 'authenticated'
 
-#define the API call to get a list of all user IDs
+#define the API call to get a list of all agent IDs
 endpoint = '//users?all_ids=true'
 
 #call the API
 ids = requests.get(baseURL + endpoint, headers=headers).json()
 
 #iterate over each returned ID, grabbing the json object
+#if downloading accessions from non-RBML repo, change the repo number below in the endpoint
 records = []
 for id in ids:
     endpoint = '//users/'+str(id)
     output = requests.get(baseURL + endpoint, headers=headers).json()
     records.append(output)
     print output
-#f=open('agentsPeople.json', 'w')
-#json.dump(records, f)
-#f.close()
 
 elapsedTime = time.time() - startTime
 m, s = divmod(elapsedTime, 60)
