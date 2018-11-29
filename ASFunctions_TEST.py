d247892a16b1dc55fa16157c694a82050086e6c1
dwhodges2 <dwhodges@gmail.com>
2018-11-29 11:26:40 -0500

    add ASFunctions lib

---
+++ ASFunctions_TEST.py
@@ -0,0 +1,10 @@
+import ASFunctions
+
+# Call functions from library...
+
+print ASFunctions.GetResource(2,4277)
+
+print ASFunctions.GetAccessions(3)
+
+print ASFunctions.GetResourceIDs(3)
+print ASFunctions.GetResources(4)
