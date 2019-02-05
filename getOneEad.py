bd4bb7505d56ce17dd3f942f3daa876c631652b3
Benjamin Armintor <armintor@gmail.com>
2019-02-05 15:00:06 -0500

    update getOneEAD for py3

--- getOneEad.py
+++ getOneEad.py
@@ -11,13 +11,16 @@ user = secrets.user
 password = secrets.password
 
 #authenticate session
-auth = requests.post(baseURL + '/users/'+user+'/login?password='+password).json()
+params = {"password" : password}
+auth = requests.post(baseURL + '/users/'+user+'/login',params=params)
+print(auth)
+auth = auth.json()
 session = auth["session"]
 headers = {'X-ArchivesSpace-Session':session, 'Content_Type':'application/json'}
-print 'authenticated'
+print ('authenticated')
 
-repo = raw_input("Enter repo ID; 2=RBML, 3=Avery, 4=Starr, 5= Burke:")
-resource = raw_input("Enter resource ID:")
+repo = input("Enter repo ID; 2=RBML, 3=Avery, 4=Starr, 5= Burke:")
+resource = input("Enter resource ID:")
 
 #define the API call to get EAD
 endpoint = '/repositories/' + repo + '/resource_descriptions/' + resource + '.xml'
@@ -26,10 +29,10 @@ endpoint = '/repositories/' + repo + '/resource_descriptions/' + resource + '.xm
 
 #call the API
 output = requests.get(baseURL + endpoint, headers=headers).text
-print output
+print( output)
 
 elapsedTime = time.time() - startTime
 m, s = divmod(elapsedTime, 60)
 h, m = divmod(m, 60)
-print 'Total script run time: ', '%d:%02d:%02d' % (h, m, s)
+print( 'Total script run time: ', '%d:%02d:%02d' % (h, m, s))
 
