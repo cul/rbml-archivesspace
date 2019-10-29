5a921a7b6d29284e34907eb7c45819fde7045e84
dwhodges2 <dwhodges@gmail.com>
2019-10-29 10:18:17 -0400

    oai harvest fix

--- oai/oai_harvest.py
+++ oai/oai_harvest.py
@@ -1,22 +1,29 @@
-import secretsDev
-import secrets
+from configparser import ConfigParser
 import subprocess
+import os
+
 
 # Script to harvest batches of OAI data from ArchivesSpace, using pyoaiharvester (https://github.com/vphill/pyoaiharvester).  
 
 
-# Set to path of pyoaiharvest.py script.
-harvester_path = 'pyoaiharvester/pyoaiharvest.py'
-
-# Set to true to harvest from dev.
+# Set to True to harvest from Dev, False to use Prod.
 dev = True
 
+
+# Set to path of pyoaiharvest.py script.
+my_path = os.path.dirname(__file__)
+harvester_path = os.path.join(my_path,'pyoaiharvester/pyoaiharvest.py')
+config_path = os.path.join(my_path, 'config.ini')
+config = ConfigParser()
+config.read(config_path)
+
+
 if dev == True:
-    oaiURL = secretsDev.baseOAIURL
-    oaiPrefix = secretsDev.oaiPrefix
+    oaiURL = config['DEV']['baseOAIURL']
+    oaiPrefix = config['DEV']['oaiPrefix']
 else:
-    oaiURL = secrets.baseOAIURL
-    oaiPrefix = secrets.oaiPrefix
+    oaiURL = config['PROD']['baseOAIURL']
+    oaiPrefix = config['PROD']['oaiPrefix']
 
 
 # Set the type of output, eg., oai_dc, oai_marc, oai_ead. 
@@ -33,7 +40,7 @@ toDate = '20180801'
 outFile = 'output_file_name.xml'
 
 
-cmd = harvester_path + ' -l ' + oaiURL +  ' -m ' + theOutputType + ' -s collection' + ' -o ' + outFile + ' -f ' + fromDate + ' -u ' + toDate
+cmd = 'python ' + harvester_path + ' -l ' + oaiURL +  ' -m ' + theOutputType + ' -s collection' + ' -o ' + outFile + ' -f ' + fromDate + ' -u ' + toDate
 
 
 # This will always work but is kludgy.
