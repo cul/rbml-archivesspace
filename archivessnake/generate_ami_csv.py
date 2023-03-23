44e61bb1aaf5a53519eb851cd5c2ca5f84c2ec64
Bonnie Gordon <blg2144@columbia.edu>
2023-03-23 17:42:29 -0400

    add command line script

---
+++ archivessnake/generate_ami_csv.py
@@ -0,0 +1,19 @@
+import argparse
+
+from scripts.update_tree import TreeUpdater
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description="Adds access restriction to archival objects without conditions governing access notes and with at least one instance."
+    )
+    parser.add_argument("mode", help="ArchivesSpace instance: dev, prod, or test.")
+    parser.add_argument(
+        "series_id", help="ArchivesSpace ID for AV series. Example: 1234",
+    )
+    args = parser.parse_args()
+    TreeUpdater(args.mode).create(args.series_id)
+
+
+if __name__ == "__main__":
+    main()
