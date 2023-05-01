a67a10eea6445c0c89b03ddd20ccc460b8b0c681
Bonnie Gordon <blg2144@columbia.edu>
2023-05-01 15:35:58 -0400

    deal with dates

--- archivessnake/scripts/update_order.py
+++ archivessnake/scripts/update_order.py
@@ -1,13 +1,18 @@
 import logging
 from configparser import ConfigParser
+from re import fullmatch
 
 from asnake.utils import walk_tree
 
 from scripts.aspace_client import ArchivesSpaceClient
 
+class DateException(Exception):
+    pass
+        
 
 class OrderUpdater(object):
     def __init__(self, series_id, mode="dev", repo_id=2):
+        # TODO: remove series_id from init
         self.series_id = series_id
         self.config = ConfigParser()
         self.config.read("local_settings.cfg")
@@ -26,13 +31,13 @@ class OrderUpdater(object):
 
     def get_wayfinders(self, stop_id, filename=None):
         """Get list of archival objects in series that have children and 2 ancestors.
-        
+
         If filename is provided, writes list to a file.
-        
+
         Args:
             stop_id (int): ID of archival object to stop traversing series tree
             filename (str): filename to write list to.
-        
+
         Returns:
             list
         """
@@ -63,7 +68,7 @@ class OrderUpdater(object):
 
     def reorder_objects(self, wayfinders_to_delete):
         """For each archival object in a list, move each child up one level.
-        
+
         Args:
             wayfinders_to_delete (list): ASpace archival object URIs
         """
@@ -79,3 +84,50 @@ class OrderUpdater(object):
                     self.as_client.aspace.client.post(
                         f"{child['uri']}/parent", params=params
                     )
+
+    def add_date_from_string(self, date_string, ao_uri):
+        date_formats = [
+            r"\d\d\d\d",
+            r"\d\d\d\d-\d\d\d\d",
+            r"\d\d\d\d-\d\d",
+            r"\d\d\d\d-\d\d-\d\d",
+        ]
+        ao_json = self.as_client.get_json(ao_uri)
+        if [True for x in date_formats if fullmatch(x, date_string)]:
+            if ao_json["dates"]:
+                logging.info(f"Dates already exist for {ao_uri}")
+            else:
+                self.create_date_object(date_string)
+        else:
+            raise Exception(f"Unexpected date format {date_string}")
+
+    def create_date_object(self, date_string):
+        date_object = {"label": "creation", "jsonmodel_type": "date"}
+        single_date_formats = [r"\d\d\d\d", r"\d\d\d\d-\d\d", r"\d\d\d\d-\d\d-\d\d"]
+        if [True for x in single_date_formats if fullmatch(x, date_string)]:
+            date_object["begin"] = date_string
+            date_object["date_type"] = "single"
+            return date_object
+        elif fullmatch(r"\d\d\d\d-\d\d\d\d", date_string):
+            begin, end = date_string.split("-")
+            date_object["begin"] = begin
+            date_object["end"] = end
+            date_object["date_type"] = "inclusive"
+            return date_object
+        else:
+            raise DateException
+            
+
+
+{"begin": }
+
+		{
+			"expression": "1920, 1961-2018",
+			"begin": "1920",
+			"end": "2018",
+			"date_type": "inclusive",
+			"label": "creation",
+			"jsonmodel_type": "date"
+		}
+
+
