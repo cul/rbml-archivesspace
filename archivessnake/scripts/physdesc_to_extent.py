c58e2830a11a09cc4bc34adbadcbda4797dba574
Celeste Brewer <30668227+dorkivist@users.noreply.github.com>
2023-08-04 16:54:39 -0400

    Update physdesc_to_extent.py
    just kidding, reverting stupid change

--- archivessnake/scripts/physdesc_to_extent.py
+++ archivessnake/scripts/physdesc_to_extent.py
@@ -26,31 +26,13 @@ class PhysdescToExtent(object):
         archival_objects = self.as_client.repositories(self.repo_id).archival_objects
         for ao in archival_objects:
             physdesc_notes = self.as_client.has_physdesc(ao)
-            if physdesc_notes and len(physdesc_notes) <= 12:
-                extent_list = physdesc_notes.strip("()").split(" ")
-            elif physdesc_notes and len(physdesc_notes) > 12:
-                # then write method to log physdesc to csv if len > 12 ?
-                # do we want this in the run method or another one?
-              
-    # this is a train wreck
-    # def folders_instance(self, ao):
-        # if getattr(ao, "container", False):
-          #  container2_folder = []
-          #  for container in ao.notes:
-          #      if type_1.lower() == "folders":
-          #         del physdesc_notes
-          #      else: update_extent
-    # if container/type_1 exists we don't want to do anything to the *container*
-    # we want to delete the physdesc note and then move on to the next ao with a physdesc note             
+            if physdesc_notes:
+                pass
 
-    # def update_extent(self, extent_list)
-        # extent_value = int(extent_list[0])
-        # for extent in [what?]:
-        #   number.append(extent_value)
-        #for extent in [what again?]:
-        #   extent_type.append(lower(extent_list[1]))
-
-
+    # identify archival objects that have physdesc notes
+    #     "notes":
+    #         "type": "physdesc"
+    #         "content": [etc etc]
 
     # identify physdesc notes that have a folder count
     # use a regex to limit to folders and ignore other physdesc notes
@@ -60,13 +42,13 @@ class PhysdescToExtent(object):
     #         if content == "folders" but ALSO some extraneous junk, parentheses check
     # account for parentheses but also cases where these may not have been added
     #     if content == "folders" and extra junk
-    #         regex to check if extra junk is *anything other than* () or )
+    #         regex to check if extra junk is anything other than () or )
     #         if junk detector = TRUE, pass
     #         if junk detector = FALSE, delete parentheses in content
     # check for instance with folder value
     #          "instances":
     #     "container": {
-    #         "type_1": "[F/folder{s}]" ! Account for variability (running .lower string method should work here)
+    #         "type_1": "[F/folder{s}]" ! Account for variability
     # do not add an extent if there is an instance with a folder value
     #     if "instances":
     #         "container": {
