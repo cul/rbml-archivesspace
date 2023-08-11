2b8c4122afa4ceae217e899c1c801557157b4cb2
Celeste Brewer <30668227+dorkivist@users.noreply.github.com>
2023-08-11 10:29:50 -0400

    Update physdesc_to_extent.py
    last week's bumbling

--- archivessnake/scripts/physdesc_to_extent.py
+++ archivessnake/scripts/physdesc_to_extent.py
@@ -26,13 +26,40 @@ class PhysdescToExtent(object):
         archival_objects = self.as_client.repositories(self.repo_id).archival_objects
         for ao in archival_objects:
             physdesc_notes = self.as_client.has_physdesc(ao)
-            if physdesc_notes:
-                pass
+            if len(physdesc_notes) <= 12:
+                extent_list = physdesc_notes.strip("()").split(" ")
+            elif len(physdesc_notes) > 12:
+                physdesc_csv(physdesc_notes)
+                # log physdesc to csv if len > 12 
 
-    # identify archival objects that have physdesc notes
-    #     "notes":
-    #         "type": "physdesc"
-    #         "content": [etc etc]
+    # def physdesc_csv(self, physdesc_notes, filename=None):
+        # with open('filename.csv', 'w', newline='') as outfile:
+            # writer = csv.writer(outfile)
+          #  key_list = list(test.keys())
+          # ugh this is gonna involve some weirdness with the ao isn't it
+
+
+            
+              
+    # this is a train wreck
+    # def folders_instance(self, ao):
+        # if getattr(ao, "container", False):
+          #  container2_folder = []
+          #  for container in ao.notes:
+          #      if type_1.lower() == "folders":
+          #         del physdesc_notes
+          #      else: update_extent
+    # if container/type_1 exists we don't want to do anything to the *container*
+    # we want to delete the physdesc note and then move on to the next ao with a physdesc note             
+
+    # def update_extent(self, extent_list)
+        # extent_value = int(extent_list[0])
+        # for extent in [what?]:
+        #   number.append(extent_value)
+        #for extent in [what again?]:
+        #   extent_type.append(lower(extent_list[1]))
+
+
 
     # identify physdesc notes that have a folder count
     # use a regex to limit to folders and ignore other physdesc notes
@@ -42,13 +69,13 @@ class PhysdescToExtent(object):
     #         if content == "folders" but ALSO some extraneous junk, parentheses check
     # account for parentheses but also cases where these may not have been added
     #     if content == "folders" and extra junk
-    #         regex to check if extra junk is anything other than () or )
+    #         regex to check if extra junk is *anything other than* () or )
     #         if junk detector = TRUE, pass
     #         if junk detector = FALSE, delete parentheses in content
     # check for instance with folder value
     #          "instances":
     #     "container": {
-    #         "type_1": "[F/folder{s}]" ! Account for variability
+    #         "type_1": "[F/folder{s}]" ! Account for variability (running .lower string method should work here)
     # do not add an extent if there is an instance with a folder value
     #     if "instances":
     #         "container": {
