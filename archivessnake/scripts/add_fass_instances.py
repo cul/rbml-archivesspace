4c4ef5313aa8628650d2532b81711d1a8eea9b53
Bonnie Gordon <blg2144@columbia.edu>
2024-07-05 10:54:48 -0400

    updates

--- archivessnake/scripts/add_fass_instances.py
+++ archivessnake/scripts/add_fass_instances.py
@@ -1,3 +1,4 @@
+import csv
 import logging
 from configparser import ConfigParser
 
@@ -35,8 +36,52 @@ class AddFassInstances(object):
         sheet_data.append(["display_string", "uri"])
         for child in tree:
             sheet_data.append([child["display_string"], child["uri"]])
+            logging.info(child["display_string"])
         write_data_to_csv(sheet_data, output_csv)
 
-    def add_instances_extents(self, subseries_csv):
+    def add_instances_extents(self, subseries_csv, add_extents=False):
         with open(subseries_csv, "r") as csvfile:
-            pass
+            reader = csv.DictReader(csvfile)
+        for row in reader:
+            try:
+                ao_uri = row["uri"]
+                ao_json = self.as_client.aspace.client.get(ao_uri).json()
+                if add_extents:
+                    extents = []
+                    for num in range(3):
+                        num += 1
+                        if row[f"phys_extent_num{num}"] and row[f"phys_extent{num}"]:
+                            extent_dict = {}
+                            extent_dict["number"] = row[f"phys_extent_num{num}"]
+                            extent_dict["extent_type"] = row[f"phys_extent{num}"]
+                            extent_dict["jsonmodel_type"] = "extent"
+                            extents.append(extent_dict)
+                    if len(extents) == 1:
+                        extents[0]["portion"] = "whole"
+                    else:
+                        for extent in extents:
+                            extent["portion"] = "part"
+                    ao_json["extents"] = extents
+                # add digital object
+                file_version_uri = row["doi"]
+                search_query = f"{file_version_uri} primary_type:digital_object"
+                response = self.as_client.aspace.repositories(2).search.with_params(
+                    q=search_query
+                )
+                results = [x for x in response]
+                if len(results) == 1:
+                    digital_object_ref = {"ref": results[0].uri}
+                    digital_object = {
+                        "instance_type": "digital_object",
+                        "jsonmodel_type": "instance",
+                        "is_representative": False,
+                        "digital_object": digital_object_ref,
+                    }
+                elif not results:
+                    pass
+                else:
+                    logging.error(
+                        f"{ao_uri}: Multiple DAOs found for {file_version_uri}"
+                    )
+            except Exception as e:
+                logging.error(f"{ao_uri}: {e}")
